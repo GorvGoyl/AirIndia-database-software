1e81a0941d20854ed14c1615a959dd3e94ee4160
Gourav Goyal <1gouravgg@gmail.com>
2015-02-26 00:16:04 +0530

    project added

---
+++ src/javaconnect.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jerry
+ */
+import java.sql.*;
+import javax.swing.*;
+public class javaconnect {
+    Connection conn=null;
+    public static Connection ConnecrDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:airport.sqlite");
+            //Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\1gour_000\\Documents\\NetBeansProjects\\Airport\\airport.sqlite");
+            // JOptionPane.showMessageDialog(null,"Connection Established :-)");
+            return conn;
+            
+        }
+        catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+                       
+        }
+    }
+    
+    
+    
+    
+    
+    
+}
